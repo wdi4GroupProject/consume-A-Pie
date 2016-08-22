4fcd106a93d63b0c3a9c2a19a7aac0c682666795
xin chen <cxin7@outlook.com>
2016-08-22 14:50:01 +0800

    allow CORs

--- config/express.js
+++ config/express.js
@@ -14,7 +14,12 @@ var config = require('./config'),
 module.exports = function() {
   var app = express();
   require('./passport')(passport); // pass passport for configuration
-
+  //cors
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
   // initialize the required module
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
