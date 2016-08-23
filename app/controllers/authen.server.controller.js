42e494284ae8993de8837c7dc207bd1238e6af8e
xin chen <cxin7@outlook.com>
2016-08-23 11:00:10 +0800

    sign up&login api

--- app/controllers/authen.server.controller.js
+++ app/controllers/authen.server.controller.js
@@ -7,13 +7,58 @@
 // var User = require('../app/models/user.model'),
 //   authConfig = require('../../config/auth');
 //
-// module.exports = function(passprot){
+// module.exports = function(passprot) {
+//   passport.serializeUser(function(user, done) {
+//     done(null, user.id);
+//   });
 //
+//   // used to deserialize the user
+//   passport.deserializeUser(function(id, done) {
+//     User.findById(id, function(err, user) {
+//       done(err, user);
+//     });
+//   });
+//   // LOCAL SIGNUP
+//   passport.use('local-signup', new LocalStrategy({
+//       usernameField: 'email',
+//       passwordField: 'password',
+//       passReqToCallback: true // allows us to pass back the entire request to the callback
+//     },
+//     function(req, email, password, done) {
+//       // asynchronous
+//       // User.findOne wont invoke unless data is sent back
+//       process.nextTick(function() {
+//         User.findOne({
+//           'local.email': email
+//         }, function(err, user) {
+//           // if there are any errors, return the error
+//           if (err)
+//             return next(err);
 //
-//
-//
-//
-//
-//
-//
+//           // check to see if theres already a user with that email
+//           if (user) {
+//             return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+//           } else {
+//             var newUser = new User();
+//             newUser.local.email = email;
+//             newUser.local.password = password;
+//             // save the user
+//             newUser.save(function(err) {
+//               if (err) {
+//                 return res.status(401).send(err);
+//               } else {
+//                 return done(null, newUser);
+//               }
+//             });
+//           }
+//         });
+//       });
+//     }));
+
+
+
+
+
+
+
 // };
--- app/routes/authen_api.routes.js
+++ app/routes/authen_api.routes.js
@@ -1,12 +1,16 @@
-// module.exports = function(app,passport){
-//   app.route('/API/signup')
-//   .post(passport.authenticate('loclocal-signup'),{
-//
-//   });
-//
-//
-//
-//
-//
-//
-// };
+module.exports = function(app,passport){
+  app.route('/API/signup')
+  .post(passport.authenticate('local-signup'),function(req,res){
+    res.json(req.session.passport.user);
+  });
+  app.route('/API/login')
+  .post(passport.authenticate('local-login'),function(req,res){
+    res.json(req.session.passport.user);
+  });
+  
+
+
+
+
+
+};
