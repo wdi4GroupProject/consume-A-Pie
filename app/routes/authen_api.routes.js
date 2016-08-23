990c3bf6e33d626b13d3294b46dcb8240bcb4559
xin chen <cxin7@outlook.com>
2016-08-23 13:16:39 +0800

    sign up and login locally ONLY

--- app/routes/authen_api.routes.js
+++ app/routes/authen_api.routes.js
@@ -7,10 +7,10 @@ module.exports = function(app,passport){
   .post(passport.authenticate('local-login'),function(req,res){
     res.json(req.session.passport.user);
   });
-  app.get('/API/auth/google',passport.authenticate('google',{scope:['profile','email']}));
-  app.get('/API/auth/google/callback',passport.authenticate('google'),function(req,res){
-    res.json(req.session.passport.user);
-  });
+  // app.get('/API/auth/google',passport.authenticate('google',{scope:['profile','email']}));
+  // app.get('/API/auth/google/callback',passport.authenticate('google'),function(req,res){
+  //   res.json(req.session.passport.user);
+  // });
 
 
 
