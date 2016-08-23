63370f51d15bdf11eb8ea31110f754eee41cd5da
yuzeng shen <yuzeng9@hotmail.com>
2016-08-23 09:31:30 +0800

    admin ver

--- app/routes/recipes.routes.js
+++ app/routes/recipes.routes.js
@@ -1,9 +1,15 @@
 module.exports =function(app){
-  var recipesController=require('../controllers/recipes.server.controller');
+  var recipesController = require('../controllers/recipes.server.controller');
   //get all recipes API url
   app.route('/API/recipes')
   .get(recipesController.all);
+
+  app.route('/recipe')
+  .get(recipesController.adminall)
+
+  app.get('/recipe/new', recipesController.new)
+
+
   //get a recipe by id API url
   app.route('/API/recipes/:id')
-  .get(recipesController.show);
-};
+  .get(recipesController.show);}
