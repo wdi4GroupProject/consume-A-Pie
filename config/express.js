39da182ec6632db00c20024ee32754ba36027b92
xin chen <cxin7@outlook.com>
2016-08-24 16:53:51 +0800

    CORs delete

--- config/express.js
+++ config/express.js
@@ -22,7 +22,7 @@ module.exports = function() {
   //cors
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
